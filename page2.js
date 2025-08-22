e61cb246c0c22beee5bccab3c7c5599bc2c9d3b4
Edward Smith <213342565+EdSmith2032@users.noreply.github.com>
2025-08-21 20:26:03 -0500

    added js for lang buttons

---
+++ page2.js
@@ -0,0 +1,8 @@
+onEvent("spanishButton","click",function(){
+setText("text", "Otra forma de ahorrar agua es ducharse más corto. ¡Prueba a programar un temporizador de 5 minutos!");
+});
+
+onEvent("englishButton","click",function(){
+setText("text", "Another way to conserve water is to take shorter showers. Try setting a timer to 5 minutes!");
+
+});
---
+++ page3.js
@@ -0,0 +1,8 @@
+onEvent("spanishButton","click",function(){
+setText("text", "En lugar de comprar botellas de agua desechables, usa una botella rellenable. Las botellas de agua de un solo uso llenan los vertederos y retienen agua valiosa.");
+});
+
+onEvent("englishButton","click",function(){
+setText("text", "Instead of buying disposable water bottles, use a refillable bottle. One use water bottles fill our landfills and trap valuable water.");
+
+});
