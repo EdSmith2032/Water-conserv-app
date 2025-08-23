7069e326cb8cfe01c13630f9bed15e80db2fa3b8
Edward Smith <213342565+EdSmith2032@users.noreply.github.com>
2025-08-22 21:18:19 -0500

    added german on main page

--- script.js
+++ script.js
@@ -6,5 +6,9 @@ onEvent("spanishButton", "click", function () {
 onEvent("englishButton", "click", function () {
     setText("heading", "Water Conservation Tips");
     setText("text", "It's important that we all do our part to use less water. Click through this app for tips for conservation ideas.");
+});
 
+onEvent("germanButton", "click", function () {
+    setText("heading", "Tipps zum Wasserschutz");
+    setText("text", "Es ist wichtig, dass wir alle unseren Teil dazu beitragen, weniger Wasser zu verbrauchen. Klicken Sie sich durch diese App und erhalten Sie Tipps zum Wassersparen.");
 });
